d353ce8520370db5913109d3136f16f0661035ed
Nouhaila Bjaghou <nbjaghou@e1r8p10.1337.ma>
2021-10-22 16:26:46 +0100

    d

--- Indexing/Indexing.cpp
+++ Indexing/Indexing.cpp
@@ -1,4 +1,5 @@
 #include "Indexing.hpp"
+
 Indexing::Indexing(std::string url): _url(url)
 {
 	_body = "<html>\n<head>\n<title>Index of " + url + "</title></head>\n<body>\n<h1>Index of " + url + "</h1>\n<hr>\n";
@@ -31,55 +32,65 @@ void Indexing::createTable()
 {
 	DIR *dr;
 	struct dirent *file;
-	std::string path;
+	std::string name;
 	dr = opendir(_url.c_str());
 	if (dr)
 	{
 		while ((file = readdir(dr)) != NULL)
 		{
-			path = (_url[_url.size() - 1] == '/') ? _url + file->d_name : _url + "/" + file->d_name;
-			_body += "<tr>\n";
-			_body += "<td style=\"width: 55%;\"><a href='";
-			_body += path;
-			_body += "'>";
-			_body += file->d_name;
-			_body += "</a></td>\n<td>";
-			_body += last_modified(path);
-			_body += "</td>\n";
-			_body += "<td>";
-			_body += "0";
-			_body += "</td>\n";
-			_body += "</tr>\n";
+			name = file->d_name;
+			if (name == "..")
+				name += "/";
+			else if (name == ".")
+				continue ;
+			_files.push_back(name);
 		}
 		closedir(dr);
+		generateTable();
+	}
+}
+
+void Indexing::generateTable()
+{
+	std::vector<std::string>::iterator it;
+	std::string path;
+	std::string size;
+	for (it = _files.begin(); it != _files.end(); it++)
+	{
+		path = (_url[_url.size() - 1] == '/') ? _url + *it : _url + "/" + *it;
+		size = SizeFile(path);
+		if (*it == "../")
+			_body = _body + "<tr>\n<td style=\"width: 55%;\"><pre><a href='" + path + "'>" + *it + "</a></pre></td>\n</tr>";
+		else
+			_body = _body + "<tr>\n<td style=\"width: 55%;\"><a href='" + path + "'>" + *it + "</a></td>\n<td>" + last_modified(path) + "</td>\n<td>" + size + "</td>\n</tr>\n";
 	}
 }
 
 std::string Indexing::last_modified(std::string path)
 {
+	struct tm* clock;
 	struct stat buff;
-	time_t rawtime;
-  	struct tm * timeinfo;
 	char buffer[100];
 
 	if (stat(path.c_str(), &buff) == 0)
 	{
-		time (&rawtime);
-  		timeinfo = localtime (&rawtime);
-		strftime(buffer, 100, "%d-%b-%Y %H:%M", timeinfo);
+		clock = gmtime(&(buff.st_mtime));
+		strftime(buffer, 100, "%d-%b-%Y %H:%M", clock);
 		return (buffer);
 	}
 	return ("");
 }
-
-int  Indexing::SizeFile(std::string path)
+std::string Indexing::SizeFile(std::string path)
 {
 	struct stat buff;
-    
+	std::string size;
+	std::stringstream ss;
+
 	if (stat(path.c_str(), &buff) == 0)
 	{
-		std::cout << buff.st_size << std::endl;
-		return buff.st_size;
+		ss << buff.st_size;    
+		ss >> size;  
+		return (size);
 	}
 	return (0);
 }
--- Indexing/Indexing.hpp
+++ Indexing/Indexing.hpp
@@ -5,8 +5,10 @@
 #include <dirent.h>
 #include <time.h>
 #include <string>
+#include <vector>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include<sstream>  
 
 class Indexing
 {
@@ -16,11 +18,13 @@ class Indexing
 		Indexing &operator=(Indexing const &rhs);
 		~Indexing();
 		void createTable();
+		void generateTable();
 		std::string last_modified(std::string path);
-		int SizeFile(std::string path);
+		std::string SizeFile(std::string path);
 		std::string getBody(void) const;
 	private:
 		std::string _url;
 		std::string _body;
+		std::vector<std::string> _files;
 };
 #endif
